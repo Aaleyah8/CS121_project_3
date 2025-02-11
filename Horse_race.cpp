ee0c80fd41e30f85ce1cb6638655bdf5593ae4af
Aaleyah8 <aaleyahveness8@gmail.com>
2025-02-11 18:19:24 +0000

    modified:   Horse_race.cpp

--- Horse_race.cpp
+++ Horse_race.cpp
@@ -3,6 +3,8 @@
 std::random_device rd;
 std::uniform_int_distribution<int> dist(0, 1);
 
+const int TRACK_LENGTH = 15;
+
 void advance(int horseNum, int* horses);
 void printLane(int horseNum, int* horses);
 bool isWinner(int horseNum, int* horses);
@@ -13,7 +15,7 @@ int main(){
 
 while (keepGoing){
 
-        for (int hn = 0; horses; hn++){
+        for (int hn = 0; hn < 5;  hn++){
                 advance(hn, horses);
                 printLane(hn, horses);
                 if (isWinner(hn, horses)){
@@ -22,6 +24,7 @@ while (keepGoing){
         } // end for
 
         std::cout << "press enter for another turn ";
+	std::cin.ignore();
 
         } // end while
 
@@ -30,16 +33,28 @@ return 0;
 
 void advance(int hn, int* horses){
         int turn = dist(rd);
+	horses[hn] += turn;
 
 } // end advance
 
 bool isWinner(int hn, int* horses){
         bool result = false;
-        for (int TRACK_LENGTH = 0; horses; hn++){
-                if (horses[hn] >= TRACK_LENGTH){
-                        result = true;
-                } // end if
-        } // end for
-return result;
+
+        if (horses[hn] >= 15){
+	       	result = true;
+		std::cout << "Horse " << hn << " WINS!" << std::ends;
+	}
+		
+	return result;
 } // end isWinner
 
+void printLane(int horseNum, int* horses){
+
+	for (int i = 0; i < TRACK_LENGTH; i++){
+		if(horses[horseNum] == i){
+			std::cout << horseNum << " ";
+		}else{ std::cout << ". ";
+		}
+	}
+	std::cout << std::ends;
+}
